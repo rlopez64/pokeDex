8e2362658518297c00a0f6bd1d2a0aea3cb19ab6
rlopez64 <136547877+rlopez64@users.noreply.github.com>
2023-09-15 10:51:13 -0700

    Formatted Code

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function fetchPokemon() {
   let pokemon = $(".pokemon").val();
   let request = new XMLHttpRequest();
@@ -7,12 +5,11 @@ function fetchPokemon() {
   let url = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
   console.log(url);
 
-  request.open("GET", url, true)
-  request.onload = function() {
-
-    if ((this.response) == "Not Found") {
-      $('#res').text('Search Result for:"' + pokemon + '"');
-      $('#pokeId').text("Pokemon not found. Please try again.");
+  request.open("GET", url, true);
+  request.onload = function () {
+    if (this.response == "Not Found") {
+      $("#res").text('Search Result for:"' + pokemon + '"');
+      $("#pokeId").text("Pokemon not found. Please try again.");
     } else {
       let data = JSON.parse(this.response);
 
@@ -27,24 +24,21 @@ function fetchPokemon() {
 
         $("#res").text('Search Result for: " ' + pokemon + ' "');
         $("#pokeId").text("This pokemon's ID in the pokedex is: " + pokeId);
-        $("#pokeWeight").text(pokemon + ' has a base unit weight of ' + pokeWeight)
-        $("#pokeHeight").text(pokemon + ' has a base unit height of ' + pokeHeight)
-        $("#pokeType").text('Type: ' + pokeType)
-        $('#front').attr('src', frontImg);
-        $('#back').attr('src', backImg);
-        $('#shiny').attr('src', shinyImg);
-
-
+        $("#pokeWeight").text(
+          pokemon + " has a base unit weight of " + pokeWeight
+        );
+        $("#pokeHeight").text(
+          pokemon + " has a base unit height of " + pokeHeight
+        );
+        $("#pokeType").text("Type: " + pokeType);
+        $("#front").attr("src", frontImg);
+        $("#back").attr("src", backImg);
+        $("#shiny").attr("src", shinyImg);
       }
-
     }
-
   };
 
   request.send();
-
-
-
 }
 
 function myFunction(imgs) {
@@ -53,22 +47,30 @@ function myFunction(imgs) {
 }
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-  'use strict'
+(function () {
+  "use strict";
 
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation')
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
 
-    // Loop over them and prevent submission
-    Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-        form.classList.add('was-validated')
-      }, false)
-    })
-  }, false)
-}())
+      // Loop over them and prevent submission
+      Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
